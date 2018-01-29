3d1778a79b05a5f943ea100342588254a2006a55
Paweł Pyrczak <pawel.pyrczak@kissdigital.pl>
2018-01-29 22:14:03 +0100

    fixes for new version of WME

--- WME-begin-end-street.user.js
+++ WME-begin-end-street.user.js
@@ -3,107 +3,30 @@
 // @description    Tool for easily centering map on node A or B of selected street
 // @namespace       http://pyrczak.pl
 // @grant          none
-// @grant          GM_info
-// @version       0.0.5
-// @match               https://editor-beta.waze.com/*editor/*
-// @match               https://www.waze.com/*editor/*
-// @author         Pawel Pyrczak '2014
+// @version       0.0.6
+// @match               https://editor-beta.waze.com/*editor*
+// @match               https://www.waze.com/*editor*
+// @author         Pawel Pyrczak '2018
 // @license         MIT/BSD/X11
 // ==/UserScript==
 
 
 /*
- * v0.0.5 - some changes made by FZ69617 
+ * v0.0.6 - quick fixes for new WME version
+ * v0.0.5 - some changes made by FZ69617
  */
 
 
 function WMEbes_bootstrap()
 {
-   var bGreasemonkeyServiceDefined = false;
-   
-   try {
-      var ver = window.navigator.appVersion.match(/Chrome\/(.*?) /)[1];
-   } catch(err) {
-      var ver = null;
-   }
-   if (null !== ver) {
-      var itschrome = true;
-      ///ver = "27.0.1438.7"; // last old working version
-      // example: 32.0.1700.107
-      // [0] - major versin
-      // [2] - minor version
-      ver = ver.split(".");
-      ver[0] = parseInt(ver[0]);
-      ver[2] = parseInt(ver[2]);
-      if (ver[0] > 27) {
-         var newmethod = true;
-      } else if (ver[0] == 27) {
-         if (ver[2] <= 1438) {
-            var newmethod = false;
-         } else {
-            var newmethod = true;
-         }
-      } else {
-         var newmethod = false;   
-      }
-   } else {
-      var itschrome = false;
-      var newmethod = false;
-   }
-
-
-   try
-   {
-      if ("object" === typeof Components.interfaces.gmIGreasemonkeyService)  // Firefox tells that "Components" is deprecated
-      {
-         bGreasemonkeyServiceDefined = true;
-      }
-    }   catch (err) { };
-
-   try
-   {
-      if  ("object" === typeof GM_info)
-      {
-         bGreasemonkeyServiceDefined = true;
-      }
-    }   catch (err) { };   
-   
-   
-   if ( "undefined" === typeof unsafeWindow  ||  ! bGreasemonkeyServiceDefined)
-   {
-      try {
-         unsafeWindow    = ( function ()
-         {
-            var dummyElem   = document.createElement('p');
-            dummyElem.setAttribute ('onclick', 'return window;');
-            return dummyElem.onclick ();
-         } ) ();
-      }
-      catch (err)
-      {
-         //Ignore.
-      }
-   }
-
-   //And check again for new chrome, and no tamper(grease)monkey
-   if ( itschrome && newmethod &&  !bGreasemonkeyServiceDefined)
-   {
       //use "dirty" but effective method with injection to document
       var DLscript = document.createElement("script");
       DLscript.textContent ='unsafeWindow=window; \n'+ // need this for compatibility
       WMEbes_init.toString()+' \n'+
       'WMEbes_init();';
       DLscript.setAttribute("type", "application/javascript");
-      document.body.appendChild(DLscript);   
-      //document.body.removeChild(DLscript);
-   } else {
-      /* begin running the code! */
-      setTimeout(WMEbes_init,500);
-   }
-   
-   
+      document.body.appendChild(DLscript);
 }
-
 
 
 function WMEbes_init() {
@@ -115,31 +38,30 @@ function WMEbes_init() {
    };
 
     WMEbes.insertButtons = function () {
-
         if (this.refreshB) {
-          if(document.getElementById('WMEbes') != null) document.getElementById('WMEbes').parentNode.removeChild(document.getElementById('WMEbes'));
-      }
-       
+            if(document.getElementById('WMEbes') != null) document.getElementById('WMEbes').parentNode.removeChild(document.getElementById('WMEbes'));
+        }
+
         if(unsafeWindow.Waze.selectionManager.selectedItems.length == 0) return;
-       
+
         try{
             if(document.getElementById('WMEbes') != null) return;
         }
         catch(e){ }
 
-        var b1 = $('<button class="btn btn-default" style="margin-left: 0px;padding-left:15px;padding-right:15px;" title="Go to segment A">A</button>');
+        var b1 = $('<button class="waze-btn waze-btn-white" style="margin-left: 0px;padding-left:15px;padding-right:15px;" title="Go to segment A">A</button>');
         b1.click(WMEbes.goBegin);
-        var b2 = $('<button class="btn btn-default" style="margin-left: 5px;padding-left:15px;padding-right:15px;" title="Go to segment B">B</button>');
+        var b2 = $('<button class="waze-btn waze-btn-white" style="margin-left: 5px;padding-left:15px;padding-right:15px;" title="Go to segment B">B</button>');
         b2.click(WMEbes.goEnd);
         if (WMEbes.isLast) {
-         var b3 = $('<button class="btn btn-default" style="margin-left: 5px;padding-left:10px;padding-right:10px;background-color:#ccffcc;" title="Go to remebered position">return</button>');
+         var b3 = $('<button class="waze-btn waze-btn-default" style="margin-left: 5px;padding-left:10px;padding-right:10px;background-color:#26bae8;" title="Go to remebered position">return</button>');
          b3.click(WMEbes.goLast);
-         var b4 = $('<button class="btn btn-default" style="margin-left: 5px;padding-left:5px;padding-right:5px;background-color:#ffcccc;" title="Delete remebered position">X</button>');
+         var b4 = $('<button class="waze-btn waze-btn-default" style="margin-left: 5px;padding-left:5px;padding-right:5px;background-color:#ffcccc;" title="Delete remebered position">X</button>');
          b4.click(WMEbes.removeLast);
       }
 
-        var c = $('<section id="WMEbes" class="attributes-form side-panel-section"/>');     
-       
+        var c = $('<section id="WMEbes" class="attributes-form side-panel-section"/>');
+
         var dGroup = $('<div class="control-group"/>');
         var dControls = $('<div class="controls"/>');
         dControls.append( $('<label class="control-label">Go to</label>') );
@@ -151,24 +73,24 @@ function WMEbes_init() {
       }
         dGroup.append(dControls);
         c.append(dGroup);
-       
-        $("#segment-edit-general").append(c);
-                   
+
+        $("#edit-panel section.tabs-container").append(c);
+
     }
 
 
     WMEbes.goBegin = function (ev) {
       WMEbes.go(ev,1);
    }
-   
+
     WMEbes.goEnd = function (ev) {
       WMEbes.go(ev,2);
    }
-   
+
     WMEbes.goLast = function( ev) {
       WMEbes.go(ev,3);
    }
-   
+
     WMEbes.removeLast = function(ev) {
       WMEbes.isLast = false;
       WMEbes.refreshB = true;
@@ -179,7 +101,7 @@ function WMEbes_init() {
 
    WMEbes.loadLS = function() {
        if (this.isLSsupported) {
-         
+
          try {
          if ("string" == typeof localStorage.WMEbes) {
             var s = JSON.parse(localStorage.WMEbes);
@@ -189,9 +111,9 @@ function WMEbes_init() {
                this.zoom = s.zoom;
             }
          }
-         
+
          } catch (err) {
-            
+
          }
       }
    }
@@ -206,14 +128,14 @@ function WMEbes_init() {
       }
    }
 
- 
+
     WMEbes.timer = function() {
       if ((unsafeWindow.Waze.selectionManager.selectedItems.length > 0) && !(document.getElementById('WMEbes') != null)) {
           WMEbes.insertButtons();
       }
    }
-   
-   
+
+
     WMEbes.go = function (ev,where) {
         var convertOK;
         var foundSelectedSegment = false;
@@ -244,7 +166,7 @@ function WMEbes_init() {
             }
                 //alert(pa.x+" "+pa.y);
                 unsafeWindow.Waze.map.setCenter([pa.x,pa.y],zoom);
-               
+
                 if ((!this.isLast) && (where != 3)) {
                this.last = [pb.lon,pb.lat];
                this.zoom = zoom;
@@ -271,7 +193,7 @@ function WMEbes_init() {
         if(typeof console != "undefined")
             console.log(msg);
     }
-   
+
 
    WMEbes.startcode = function () {
       // Check if WME is loaded, if not, waiting a moment and checks again. if yes addiding event to selectionManager
@@ -294,11 +216,11 @@ function WMEbes_init() {
             setTimeout(WMEbes.startcode, 400);
       }
    }
-   
-   
+
+
    WMEbes.startcode();
 
-   
+
 }
 
 
